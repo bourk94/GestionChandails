77804a062e0dcee7764ece1f11643cd541468209
Fridriver <117280045+Fridriver@users.noreply.github.com>
2023-04-02 18:11:55 -0400

    Mise en forme de la page d'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 @section('contenu')
 
-    <h1>Accueil</h1>
+<div class="padding">
+    <h2 class="center">
+        Campagne d’hiver 2023 [en cours]
+        <br>
+        Intention d’achats disponible du X au X
+    </h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris nulla, sagittis ac nisl in, elementum euismod nunc. Curabitur eget mauris non nibh lacinia hendrerit.</p>
+    <h4>Donec dapibus maximus rutrum.</h4>
+</div>
+    
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="/css/style.css">
+    
     <title>Document</title>
 </head>
 <body>
 
-<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
+<ul class="topnav">
+  <li><a class="active" href="#logo">Logo</a></li>
+  <li><a href="#option1">Option1</a></li>
+  <li class="right"><a href="#connexion">Connexion</a></li>
+  <li class="right"><a href="#outils">Outils</a></li>
 </ul>
 
     @yield('contenu')
