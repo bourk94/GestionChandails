4e6c84be868e62c7a4c19082e8fe58711f423835
Fridriver <117280045+Fridriver@users.noreply.github.com>
2023-04-04 08:48:12 -0400

    Voici un bouton et des autres trucs dans la page d'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -7,8 +7,13 @@
         <br>
         Intention d’achats disponible du X au X
     </h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris nulla, sagittis ac nisl in, elementum euismod nunc. Curabitur eget mauris non nibh lacinia hendrerit.</p>
-    <h4>Donec dapibus maximus rutrum.</h4>
+
+    <div class="bckgroundObjets">
+        <h3>Titre de la campagne</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mauris nulla, sagittis ac nisl in, elementum euismod nunc. Curabitur eget mauris non nibh lacinia hendrerit.</p>
+        <h4>Donec dapibus maximus rutrum.</h4>
+        <a href="#" class="button">Voici un bouton</a>
+    </div>
 </div>
     
 
