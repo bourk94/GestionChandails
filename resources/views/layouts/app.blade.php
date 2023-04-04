509e9eb51ece5a3bb298ae98c33449ac46c132ca
bourk94 <113631191+bourk94@users.noreply.github.com>
2023-04-04 11:33:39 -0400

    Continuation page création campagne

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
 <body>
 
 <ul class="topnav">
-  <li><a class="active" href="#logo">Logo</a></li>
-  <li><a href="#option1">Option1</a></li>
+  <li><a class="active" href="/">Logo</a></li>
+  <li><a href="/campagnes/create">Créer campagne</a></li> {{-- Temporaire pour le moment --}}
   <li class="right"><a href="#connexion">Connexion</a></li>
   <li class="right"><a href="#outils">Outils</a></li>
 </ul>
