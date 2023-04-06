203302d11dfac7b1d04860dd7560aba2157c9094
ABlaisOuellette <alexandreouellette91@gmail.com>
2023-04-06 16:39:59 -0400

    Push models
    Push des modèles
    
    - il manque le modèle article_campagne
    - il manque le modèle article_commande
    - il manque le modèle client_commande
    
    - ajout du seeder pour SuperAdmin
    
    - le seeder par défaut DatabaseSeeder fonctionne.

--- app/Models/Campagne.php
+++ app/Models/Campagne.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Campagne extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nom_campagne', 'date_debut_campagne', 'date_fin_campagne', 'date_debut_collecte', 'date_fin_collecte', 'progression', 'statut'];
 }
--- database/seeders/SuperAdminsTableSeeder.php
+++ database/seeders/SuperAdminsTableSeeder.php
@@ -6,18 +6,18 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use DB;
 
-class Articles_CommandesTableSeeder extends Seeder
+class SuperAdminsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('article_commande')->insert(
+        DB::table('superadmins')->insert(
             [
                 [
-                    'article_id' => '1',
-                    'commande_id' => '1',
+                    'nom' => 'compteSuperAdmin',
+                    'password' => 'Secret1234',
                     'created_at'=> date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s')
                 ],
