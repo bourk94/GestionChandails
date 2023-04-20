6d4a7c88787e172b9f1980585ad09717ab7cb050
ABlaisOuellette <alexandreouellette91@gmail.com>
2023-04-20 16:12:36 -0400

    Push Formulaire ajout de couleur et taille
    - Ajout du formulaire ajout de couleur et taille

--- resources/views/Couleurs/createCouleur.blade.php
+++ resources/views/Couleurs/createCouleur.blade.php
@@ -1,55 +1,33 @@
 @extends('layouts.app')
 
-@section('title', 'Créer un article')
+@section('title', 'Ajouter une couleur')
 @section('contenu')
 
-    <form id="my-form" action="{{ route('articles.store') }}" method="POST" enctype="multipart/form-data">
+    <form id="form_couleur" action="{{ route('couleurs.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="card__padding">
             <div class="card__container">
                 <div class="flex__center">
                     <div>
-                        <h2>Créer un article</h2>
+                        <h2>Ajouter une couleur</h2>                        
                         <div>
-                            <label for="imageArticle">image de l'article</label>
-                            <input type="file" class="form-control @error('image') is-invalid @enderror"
-                                class="form-control-file" id="image" name="image">
-                        </div>
-                        <div>
-                            <label for="nomArticle">Nom de l'article</label>
-                            <input type="text" name="nom" id="nom">
+                            <label for="nomCouleur">Nom de la couleur</label>
+                            <input type="text" name="nom_couleur" id="nom_couleur">
 
-                            @error('nom')
+                            @error('nom_couleur')
                                 <span class="text-danger">{{ $messages }}</span>
                             @enderror
 
                         </div>
                         <div>
-                            <label for="typeArticle">type de l'article</label>
-                            <select name="type" id="type">
+                            <label for="codeCouleur">Code de la couleur</label>
+                            <input type="text" name="code_couleur" id="code_couleur" placeholder="Ex: FFFFFF">            
 
-                                <option value="Chandail">Chandail</option>
-
-                                <option value="Kangourou">Kangourou</option>
-
-                                <option value="Accessoire">Accessoire</option>
-                                
-                            </select>
-
-
-                            @error('type')
+                            @error('code_couleur')
                                 <span class="text-danger">{{ $messages }}</span>
                             @enderror
 
-                        </div>
-
-                        <!--EST CE QUE L'ON GÈRE LES COULEURS À PARTIR D'ICI ???-->
-                        <!--
-                                <div>
-                                    <label for="nomCouleur">Couleur</label>
-                                </div>
-                             -->
-
+                        </div>                        
                     </div>
                 </div>
             </div>
@@ -69,7 +47,57 @@
                 </template> -->
 
         <div class="flex__center margin__top">
-            <button class="btn bg__orange color__white" type="submit">Ajouter un article</button>
+            <button class="btn bg__orange color__white" type="submit">Ajouter une couleur</button>
+        </div>
+
+    </form>
+
+    <!--Form pour l'ajout d'une taille-->
+    <form id="form_taille" action="{{ route('tailles.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="card__padding">
+            <div class="card__container">
+                <div class="flex__center">
+                    <div>
+                        <h2>Ajouter une taille</h2>                        
+                        <div>
+                            <label for="nomCouleur">Nom de la couleur</label>
+                            <input type="text" name="nom_couleur" id="nom_couleur">
+
+                            @error('nom_couleur')
+                                <span class="text-danger">{{ $messages }}</span>
+                            @enderror
+
+                        </div>
+                        <div>
+                            <label for="codeCouleur">Code de la couleur</label>
+                            <input type="text" name="code_couleur" id="code_couleur" placeholder="Ex: FFFFFF">            
+
+                            @error('code_couleur')
+                                <span class="text-danger">{{ $messages }}</span>
+                            @enderror
+
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <template id="my-template">
+                    <div class="card__padding">
+                        <div class="card__container">
+                            <div class="flex__center">
+                                <div>
+                                    <label for="nomArticle">Nom de l'article</label>
+                                    <input name="nomArticle" type="text"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </template> -->
+
+        <div class="flex__center margin__top">
+            <button class="btn bg__orange color__white" type="submit">Ajouter une couleur</button>
         </div>
 
     </form>
