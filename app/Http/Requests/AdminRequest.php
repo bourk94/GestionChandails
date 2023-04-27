f360d15388ff4331b15b5753b2ffb734afaa5db0
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2023-04-27 12:08:48 -0400

    Cherry-Pick Request Admin

--- app/Http/Requests/AdminRequest.php
+++ app/Http/Requests/AdminRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class AdminRequest extends FormRequest
 {
@@ -23,9 +24,10 @@ class AdminRequest extends FormRequest
     public function rules(): array
     {
         return [     
-            'email'=>['required','email','unique:usagers', 'regex:/([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)@cegeptr\.qc\.ca/'],
+            'email'=>['required','email','unique:usagers', /* 'regex:/([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)@cegeptr\.qc\.ca/' */],
             'nom'=>'required|min:2|max:30',
-            'prenom'=>'required|min:2|max:30'
+            'prenom'=>'required|min:2|max:30',
+            'password'=> [($this->id ? 'required' : 'prohibited'),'confirmed', 'max:64', Password::min(8)->letters()->mixedCase()->numbers()->symbols()->uncompromised()],
         ];
     }
 
@@ -41,6 +43,10 @@ class AdminRequest extends FormRequest
             'prenom.required'=>'Le prénom est obligatoire',
             'prenom.min'=>'Le prénom doit faire au moins 2 caractères',
             'prenom.max'=>'Le prénom doit faire au plus 30 caractères',
+            'password.required'=>'Le mot de passe est obligatoire',
+            'password.min'=>'Le mot de passe doit faire au moins 8 caractères',
+            'password.max'=>'Le mot de passe doit faire au plus 64 caractères',
+            'password.confirmed'=>'Les mots de passe ne correspondent pas',
         ];
 
     }
