f4569fcb64e213f05d89b2e58a7533296ead7157
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2023-04-28 16:33:41 -0400

    Affichage fonctionnel

--- resources/views/Accueil/show.blade.php
+++ resources/views/Accueil/show.blade.php
@@ -1,68 +1,143 @@
 @extends('layouts.app')
 @section('contenu')
 
-<div class="padding">
-            <!-- Section informations de la campagne -->
-            <h2 class="center">
-                Campagne
-                @foreach($campagnes as $campagne)
-                    @if($campagne->statut == 'en cours')
-                        {{$campagne->nom_campagne}}
-                        [{{$campagne->statut}}]
-                        <br>
-                        {{$campagne->progression}} disponible
-                        <br>
-                        du {{$campagne->date_debut_campagne}} au {{$campagne->date_fin_campagne}}
-                    @endif
-                @endforeach
-            </h2>
-
-            @if(count($articles))
-                @foreach($articles as $article)
-                    <div class="bckgroundObjets">
+    <!-- <div class="row"> -->
+    <div class="padding">
+        <!-- Section informations de la campagne -->
+        <h2 class="center">
+            Campagne
+            @foreach ($campagnes as $campagne)
+                @if ($campagne->statut == 'en cours')
+                    {{ $campagne->nom_campagne }}
+                    [{{ $campagne->statut }}]
+                    <br>
+                    Est en : {{ $campagne->progression }}
+                    <br>
+                    du {{ $campagne->date_debut_campagne }} au {{ $campagne->date_fin_campagne }}
+                @endif
+            @endforeach
+        </h2>
+        <!-- Section qui affiche les articles disponibles dans la campagne avec les couleurs et les tailles -->
+        @if (count($articles))
+            @foreach ($articles as $article)
+                @if ($article->quantite > 0)
+                    <div class="bckgroundObjets ">
                         <div class="leftObjets">
-                            <div class="zoneImageVide"></div>
+                            @if ($article->image == 'null')
+                                <div class="zoneImageVide"></div>
+                            @else
+                                <img src="{{ asset("img/$article->type/$article->image") }}" alt="Image de l'article">
+                            @endif
                         </div>
                         <div class="rightObjets">
-                            <h2>{{$article->nom}}</h2>
+                            <h2>{{ $article->nom }}</h2>
+                            <p>Description:{{ $article->description }}</p>
+                            <p>Estimé de prix:{{ $article->prix }} $</p>
+                            <p>Nombre de {{ $article->nom }} que vous pouvez commander: {{ $article->quantite }}</p>
                         </div>
 
                         <!-- Section choix de couleurs -->
                         <div class="rightObjets">
-                            @if(count($couleurs))
-                                @foreach($couleurs as $couleur)
-                                    <label class="{{$couleur->nom_couleur}}">
-                                        <input type="radio" name="color" value="{{$couleur->nom_couleur}}" class="radNone">
-                                        <div class="button"><span></span></div>
-                                    </label>
-                                @endforeach
-                            @endif
-                            <script> 
+
+                            <!-- affichage simple-->
+                            {{-- <label class="{{ $article->nom_couleur }}">
+                            <input type="radio" name="{{ $article->nom }}color" value="{{ $article->nom_couleur }}"
+                                class="radNone">
+                            <div class="button"><span></span></div>
+                        </label> --}}
+
+                            <!-- Affichage multiple -->
+
+
+
+                            @foreach ($couleurs->where('article_id', 'like', $article->article_id) as $couleur)
+                                <label class="{{ $couleur->nom_couleur }}">
+                                    <input type="radio" name="color" value="{{ $couleur->nom_couleur }}"
+                                        class="radNone">
+                                    <div class="button"><span></span></div>
+                                </label>
+                            @endforeach
+
+
+                            <script>
                                 var couleurs = document.createElement('style');
-                                couleurs.innerHTML = `@foreach($couleurs as $couleur) .{{$couleur->nom_couleur}} .button span { background-color: {{$couleur->code_couleur}}; } @endforeach`;
+                                couleurs.innerHTML =
+                                    `@foreach ($couleurs as $couleur) .{{ $couleur->nom_couleur }} .button span { background-color: {{ $couleur->code_couleur }}; } @endforeach`;
                                 document.head.appendChild(couleurs);
                             </script>
                         </div>
-                            
+
                         <!-- Section choix de taille -->
                         <div class="rightObjets">
-                            @if(count($tailles))
-                                @foreach($tailles as $taille)
-                                    <label>
-                                        <input type="radio" name="size" value="{{$taille->format}}" class="radNone">
-                                        <div class="button"><span>{{$taille->format}}</span></div>
-                                    </label>
-                                @endforeach
-                            @endif
+                            <!-- affichage simple-->
+                            <label>
+                                <input type="radio" name="{{ $article->nom }}size" value="{{ $article->format }}"
+                                    class="radNone">
+                                <div class="button"><span>{{ $article->format }}</span></div>
+                            </label>
+
+                            <!-- Affichage multiple-->
+                            {{-- @if (count($tailles))
+                            @foreach ($tailles as $taille)
+                                <label>
+                                    <input type="radio" name="size" value="{{ $taille->format }}" class="radNone">
+                                    <div class="button"><span>{{ $taille->format }}</span></div>
+                                </label>
+                            @endforeach
+                        @endif --}}
+                        </div>
+                        <div>
+
+
+
+                            {{-- non-fonctionnel <label>
+                            <button id="gestion{{ $article->nom }}addition">+</button>
+                            <span id="{{ $article->nom }}input">0</span>
+                            <button id="gestion{{ $article->nom }}soustraction">-</button>
+                        </label> --}}
+
+                            <!-- JavaScript  qui limite la quantité d'un article dans plusieurs inputs-->
+
+
                         </div>
                         <div class="rightObjets">
                             <a href="#" class="buttonSite">Ajouter au panier</a>
                         </div>
                     </div>
-                @endforeach
-            @else
-                <p>Aucun article</p>
-            @endif
-        </div>
+                @endif
+            @endforeach
+        @else
+            <p>Aucun article</p>
+        @endif
+        {{-- non fonctionnel
+            <script>
+            let inputkangourou = document.getElementById("kangourousinput");
+            let addition = document.getElementById("gestionkangourousaddition");
+            let soustraction = document.getElementById("gestionkangouroussoustraction");
+            let valeur = 0;
+            const max = {{ $article->quantite }};
+            const min = 0;
 
+            addition.addEventListener("click", function() {
+                if (valeur >= max) {
+                    valeur = max;
+                    
+                } else {
+                    valeur++;
+                    inputkangourou.textContent = valeur;
+                }
+            });
+
+            soustraction.addEventListener("click", function() {
+                if (valeur <= min) {
+                    valeur = 0; 
+                } else {
+                       valeur--;
+                    inputkangourou.textContent = valeur;
+                }
+
+            });
+        </script> --}}
+
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">    
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>@yield('title')</title>
 </head>
 
 <body>
 
-<ul class="topnav">
-    <li><a class="active" href="{{ route('accueil') }}">Logo</a></li>
 
-    <ul class="topnav">
-        <li><a class="active" href="/">Logo</a></li>
+        
+
+        <ul class="topnav">
+            <li><a class="active" href="{{ route('accueil') }}">Logo</a></li>
 
             <!-- Dropdown pour gérer les campagnes -->
             <li class="dropdown">
@@ -27,68 +27,81 @@
                 </div>
             </li>
 
-                <!-- Dropdown pour gérer les articles -->
-                <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">Article</a>
-                    <div class="dropdown-content">
-                        <a href="#">Créer</a>
-                        <a href="#">Modifier</a>
-                        <a href="#">Supprimer</a>
-                    </div>
-                </li>
-            
-       
-        <!--  -->
-
-        <!-- Dropdown pour le super administrateur -->
-        @auth
-            @if (Auth::user()->type == 'superadmin')
-                <!-- Dropdown pour gérer les utilisateurs -->
-                <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">Utilisateur</a>
-                    <div class="dropdown-content">
-                        <a href="#">Créer</a>
-                        <a href="#">Modifier</a>
-                        <a href="#">Supprimer</a>
-                    </div>
-                </li>
-            @endif
-        @endauth
-        <!--  -->
-
-        <!-- Boutons connexion et déconnexion -->
-        @if (!Auth::user())
-            <li class="right"><a href="/usagers/login">Connexion</a></li>
-        @endif
-
-        @if (Auth::user())
-            <li class="right">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button class="test" type="submit">Déconnecter</button>
-                </form>
+            <!-- Dropdown pour gérer les articles -->
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Article</a>
+                <div class="dropdown-content">
+                    <a href="#">Créer</a>
+                    <a href="#">Modifier</a>
+                    <a href="#">Supprimer</a>
+                </div>
             </li>
-        @endif
-        <!--  -->
-        @if (Auth::user())
-            <li class="right"><a href="#">
-                    {{-- @if ($usager->id == Auth::user()->id)
+
+
+            <!--  -->
+
+            <!-- Dropdown pour le super administrateur -->
+            @auth
+                @if (Auth::user()->type == 'superadmin')
+                    <!-- Dropdown pour gérer les utilisateurs -->
+                    <li class="dropdown">
+                        <a href="javascript:void(0)" class="dropbtn">Utilisateur</a>
+                        <div class="dropdown-content">
+                            <a href="#">Créer</a>
+                            <a href="#">Modifier</a>
+                            <a href="#">Supprimer</a>
+                        </div>
+                    </li>
+                @endif
+            @endauth
+            <!--  -->
+
+            <!-- Boutons connexion et déconnexion -->
+            @if (!Auth::user())
+                <li class="right"><a href="/usagers/login">Connexion</a></li>
+            @endif
+
+            @if (Auth::user())
+            <li class="dropdown right">
+                <a href="javascript:void(0)" class="dropbtn">{{ Session::get('user') }}</a>
+                <div class="dropdown-content">
+                    <a href="{{ route('usagers.edit') }}">Mon compte</a>
+                    </form>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit">Déconnecter</button>
+                    </form>
+                </div>
+            </li>
+            @endif
+            <!--  -->
+            @if (Auth::user())
+                <li class="right"><a href="#">
+                        {{-- @if ($usager->id == Auth::user()->id)
                 {{$usager->type}}
             @endif --}}
-                </a></li>
-        @endif
+                    </a></li>
+            @endif
 
-    <!-- Boutons connexion et déconnexion -->
-    @if (!Auth::user())
-        <li class="right"><a href="{{ route('usagers.login') }}">Connexion</a></li>
+            <!-- Boutons connexion et déconnexion -->
+            @if (!Auth::user())
+                <li class="right"><a href="{{ route('usagers.login') }}">Connexion</a></li>
+            @endif
+
         </ul>
-    @endif
+     
+        <!--  -->
+
+
+
+    @yield('contenu')
+
     @if (Session::has('message'))
         <div class="container alert alert-success text-center">
 
             <h3>{{ Session::get('message') }}</h3>
 
-            </div>
+        </div>
     @endif
 
     @if (isset($messages) && $errors->any())
@@ -97,25 +110,7 @@
                 <p>{{ $error }}</p>
             @endforeach
         </div>
-        @endif
-    @if (Auth::user())
-    <li class="dropdown right">
-        <a href="javascript:void(0)" class="dropbtn">{{ Session::get('user') }}</a>
-        <div class="dropdown-content">
-            <a href="{{ route('usagers.edit')}}">Mon compte</a>
-        </form>
-            <form method="POST" action="{{ route('logout') }}" >
-            @csrf
-            <button type="submit">Déconnecter</button>
-        </form>
-        </div>
-    </li>
     @endif
-    <!--  -->
-</ul>
-
-    @yield('contenu')
-
     <!--IL MANQUE LE CODE POUR LES MESSAGES D'ERREURS-->
 </body>
 
