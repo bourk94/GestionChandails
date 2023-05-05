b14363c0b69e44092da1ac5883ad02ed67db5244
Fridriver <117280045+Fridriver@users.noreply.github.com>
2023-05-05 10:18:55 -0400

    CSS page superadmin et petit changement CSS en général

--- resources/views/Usagers/login.blade.php
+++ resources/views/Usagers/login.blade.php
@@ -4,7 +4,9 @@
 @section('contenu')
 
     <div class="w3-content" style="max-width:1100px;margin-top:80px;margin-bottom:80px">
+        
         <h1 class="center">Connexion</h1>
+
         <form class="w3-container" method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -13,7 +15,7 @@
                 <input class="w3-input w3-border w3-hover-border-black" style="width:100%;" type="email" id="email" name="email" value="{{ old('email') }}" required>
             </div>
             <div class="w3-section">
-                <label for="password">Mot de passe</label>
+                <label for="password">Mot de passe :</label>
                 <input class="w3-input w3-border w3-hover-border-black" style="width:100%;" type="password" id="password" name="password" required>
             </div>
         
@@ -22,12 +24,11 @@
             </div>
             <br>
             <div>
-                <button type="submit" class="w3-button w3-block w3-black">Connexion</button>
+                <button type="submit" class="w3-button w3-block w3-hover-red btnColor">Connexion</button>
                 <br>
-                <a href="{{ route('password.request') }}"><button class="w3-button w3-block w3-black" type="button">Mot de passe oublié</button></a>
+                <a class="a_decoration_none" href="{{ route('password.request') }}"><button class="w3-button w3-block w3-hover-red btnColor" type="button">Mot de passe oublié</button></a>
             </div>
         </form>
-
     </div>
 
 @endsection
